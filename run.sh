7c66747b4801508b9381336e263823f09c7e3d04
Ruslan Askerov <53179636+Ironrosh@users.noreply.github.com>
2026-06-18 17:24:42 +0300

    Feature/linux/windown run sh/bat python isolation (#2)
    * feat: isolating system python environment from project's requirements. Creating and using a venv instead
    
    * fix: removing cached dependencies for *nix systems; initial run.bat feature for system python env isolation
    
    * fix: run.bat added check for a created python env
    
    * fix: bin -> Scripts for run.bat in venv path
    
    ---------
    
    Co-authored-by: Ruslan Askerov <askerov.r.2709@gmail.com>
    Co-authored-by: Ruslan Askerov <rosh@Ruslans-MacBook-Pro.local>

--- run.sh
+++ run.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-python3 -m pip install -r requirements.txt --quiet
-python3 main.py
+
+
+PYTHON_COMMAND="python3" #Change this to your python command / path to python binary
+
+VENV_NAME="Open_PS_Calc_venv"
+
+DIR_NAME=$(dirname "$0")
+
+PYTHON_MAJVER=$($PYTHON_COMMAND -V | awk -F '.' '{ print $1 }' | awk -F ' ' '{ print $2 }')
+PYTHON_MINVER=$($PYTHON_COMMAND -V | awk -F '.' '{ print $2 }')
+
+if (( "$PYTHON_MAJVER" == 3 && "PYTHON_MINVER" >= 13 )); then
+	echo "Python version that meets the requirements found: Python $PYTHON_MAJVER.$PYTHON_MINVER"
+	
+	cd $DIR_NAME
+
+	if [ ! -d "./$VENV_NAME" ]; then
+		$PYTHON_COMMAND -m venv create $VENV_NAME
+		./$VENV_NAME/bin/python -m pip install --no-cache-dir -r requirements.txt
+	fi
+
+	PYTHON_COMMAND="./$VENV_NAME/bin/python"
+	$PYTHON_COMMAND main.py
+else
+	echo "Required python version was not found. Aborting..."
+fi
